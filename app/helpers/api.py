d5c31714ba17d4f1d132b1754624255a42a077f4
Drew <drew@broadley.org.nz>
2023-10-04 23:20:46 +1300

    Pushing fixes from app to qa

--- app/helpers/api.py
+++ app/helpers/api.py
@@ -5,60 +5,62 @@ from urllib.parse import urlencode
 import string
 import random
 import requests
+from fastapi import HTTPException
 
-from oauthlib.oauth2 import TokenExpiredError, MobileApplicationClient, WebApplicationClient
-from requests_oauthlib import OAuth2Session
-from auth0.v3.authentication.token_verifier import TokenVerifier, AsymmetricSignatureVerifier
+from authlib.integrations.starlette_client import OAuth, OAuthError
 from starlette.responses import RedirectResponse
+from fastapi import FastAPI, Depends, HTTPException, status, Request
 
 from app import context_external_user, context_external_user_token
 
 
 def get_client(token: Optional[str] = None):
+    oauth = OAuth()
 
-    token_endpoint_url = f"https://{os.getenv('AUTH0_DOMAIN')}{os.getenv('AUTH0_ENDPOINT_TOKEN')}"
-
-    client_token = MobileApplicationClient(
-        client_id=os.getenv("AUTH0_CLIENT_ID")
-    )
-
-    client_token = WebApplicationClient(
-        client_id=os.getenv("AUTH0_CLIENT_ID")
-    )
-
-    auth0 = OAuth2Session(
-        client=client_token,
-        scope="openid email profile",
+    oauth.register(
+        name='auth0',
+        client_id=os.getenv("AUTH0_CLIENT_ID"),
+        client_secret=os.getenv("AUTH0_CLIENT_SECRET"),
+        authorize_url=f"https://{os.getenv('AUTH0_DOMAIN')}{os.getenv('AUTH0_ENDPOINT_AUTH')}",
+        authorize_params=None,
+        access_token_url=f"https://{os.getenv('AUTH0_DOMAIN')}{os.getenv('AUTH0_ENDPOINT_TOKEN')}",
         redirect_uri=os.getenv("AUTH0_CALLBACK"),
-        token=token,
-        auto_refresh_url=token_endpoint_url,
-        auto_refresh_kwargs={
-            "client_id": os.getenv("AUTH0_CLIENT_ID"),
-            "client_secret": os.getenv("AUTH0_CLIENT_SECRET")
+        client_kwargs={
+            'scope': 'openid profile email'
         },
-        token_updater=token_save
+        server_metadata_url=os.getenv('AUTH0_CONF')
     )
 
-    return auth0
+    return oauth
 
 
-def login(callback=None, state=None):
+def get_jwks_keys():
+    metadata_url = os.getenv('AUTH0_KEYS')
+    metadata = requests.get(metadata_url).json()
+    jwks_keys = metadata.get("jwks_uri")
+    return jwks_keys
 
+
+def login(callback=None):
+    redirect_uri = os.getenv("AUTH0_CALLBACK")
+    return redirect_uri
+
+    """
     client = get_client()
     auth_endpoint_url = f"https://{os.getenv('AUTH0_DOMAIN')}{os.getenv('AUTH0_ENDPOINT_AUTH')}"
     authorization_url, state = client.authorization_url(
         auth_endpoint_url,
         state=state,
         prompt="login",
-        #response_mode="form_post",
+        # response_mode="form_post",
         audience=os.getenv('AUTH0_AUDIENCE')
     )
 
     return authorization_url
+    """
 
 
 def token_get(code: str):
-
     client = get_client()
     token_endpoint_url = f"https://{os.getenv('AUTH0_DOMAIN')}{os.getenv('AUTH0_ENDPOINT_TOKEN')}"
     token = client.fetch_token(
@@ -71,7 +73,6 @@ def token_get(code: str):
 
 
 def token_refresh(refresh_token: str):
-
     client = get_client()
     token_endpoint_url = f"https://{os.getenv('AUTH0_DOMAIN')}{os.getenv('AUTH0_ENDPOINT_TOKEN')}"
     token = client.refresh_token(
@@ -87,12 +88,11 @@ def token_save(token: str):
 
 
 def request(url: str, type: Optional[str] = "GET", data: Optional[dict] = {}):
-
     token = context_external_user_token.get()
     headers = {"Authorization": f"Bearer {token}"}
-    ssl_verify = bool(strtobool(os.getenv("AB_API_SSL_VERIFY")))
+    ssl_verify = bool(strtobool(os.getenv("UB_API_SSL_VERIFY")))
 
-    url_full = os.getenv("AB_API_BASE") + url
+    url_full = os.getenv("UB_API_BASE") + url
 
     try:
         if type == "GET":
@@ -103,14 +103,13 @@ def request(url: str, type: Optional[str] = "GET", data: Optional[dict] = {}):
         return response.json()
 
     except requests.exceptions.ConnectionError as e:
-        RedirectResponse("/error")
+        raise HTTPException(status_code=500, detail="API isn't responding")
 
     except requests.exceptions.HTTPError as e:
-    #except TokenExpiredError as e:
+        # except TokenExpiredError as e:
         token = token_refresh(token=token)
         return request(url=url, token=token, type=type, data=data)
 
 
 def logout(callback: str):
-
     return callback
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -13,28 +13,30 @@ from app.helpers.templates import templates
 router = APIRouter(
     prefix="/auth",
     tags=["auth"],
-    #dependencies=[Depends(get_token_header)],
+    # dependencies=[Depends(get_token_header)],
     responses={404: {"description": "Not found"}},
 )
 
 
-#@router.get("/", response_model=list[schemas.UserView])
-#async def user_get_all(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+# @router.get("/", response_model=list[schemas.UserView])
+# async def user_get_all(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 #    users = crud.user_get_all(db, skip=skip, limit=limit)
 #    return users
 
 
 @router.get("/login", response_class=HTMLResponse)
 async def auth_login(
-    request: Request,
-    state: Optional[str] = None,
-    redirect: Optional[str] = None
+        request: Request,
+        state: Optional[str] = None,
+        redirect: Optional[str] = None
 ):
+    client = api.get_client()
+
     if state is not None:
         request.session['auth_state'] = state
         request.session['auth_redirect'] = redirect
-        redirect_url = api.login(state=state)
-        return RedirectResponse(redirect_url)
+        redirect_uri = api.login()
+        return await client.auth0.authorize_redirect(request=request, redirect_uri=redirect_uri, state=state)
     else:
         # Get the client to set the state
         return templates.TemplateResponse("pages/auth/login.html", {"request": request})
@@ -45,24 +47,33 @@ async def auth_callback(
         request: Request,
         code: str
 ):
+    state_from_session = request.session.get("auth_state")
+    state_from_callback = request.query_params.get("state")
+    if not state_from_session or state_from_callback != state_from_callback:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid state parameter")
 
-    token = api.token_get(code)
+    client = api.get_client()
+
+    try:
+        token = await client.auth0.authorize_access_token(request)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Failed to fetch access token")
+
     request.session['auth_access_token'] = token['access_token']
     request.session['auth_id_token'] = token['id_token']
     request.session['auth_scope'] = json.dumps(token['scope'])
     request.session['auth_expires'] = datetime.fromtimestamp(token['expires_at']).strftime("%Y-%m-%d %H:%M:%S")
 
-    #return RedirectResponse(request.session['auth_redirect'])
-    return templates.TemplateResponse("pages/auth/callback.html", {"request": request})
+    # return RedirectResponse(request.session['auth_redirect'])
+    return templates.TemplateResponse("pages/auth/callback.html", {"request": request, "env": os.getenv})
 
 
 @router.post("/callback", response_class=HTMLResponse)
 async def auth_callback(
         request: Request
 ):
-
-    #token = api.token_get(code)
-    #return token
+    # token = api.token_get(code)
+    # return token
 
     request_body = await request.body()
     access_token = parse.parse_qs(request_body.decode("utf-8"))
@@ -73,14 +84,14 @@ async def auth_callback(
         request.session['auth_token'] = access_token
         return RedirectResponse(request.session.get('auth_redirect'))
 
-    #return templates.TemplateResponse("pages/auth/callback.html", {"request": request})
+    # return templates.TemplateResponse("pages/auth/callback.html", {"request": request})
 
 
 @router.get("/logout", response_class=HTMLResponse)
 async def auth_logout(
-    request: Request
+        request: Request
 ):
-
+    redirect_logout = parse.quote(f"{os.getenv('UB_WWW_BASE')}/logged-out", safe='')
     request.session.clear()
-    return RedirectResponse("/")
-
+    redirect_uri = f"https://{os.getenv('AUTH0_DOMAIN')}/v2/logout?returnTo={redirect_logout}&client_id={os.getenv('AUTH0_CLIENT_ID')}"
+    return RedirectResponse(redirect_uri)
